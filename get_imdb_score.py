df5f7767fa410228eeb1b4cff872d5b25cd9f3e8
RIAdrian <adrian.rentea24@gmail.com>
2023-09-09 15:25:49 +0300

    commit_2

--- get_imdb_score.py
+++ get_imdb_score.py
@@ -7,10 +7,10 @@ api_key = 'YOUR_OMDB_API_KEY'
 # Read the original CSV file
 df = pd.read_csv('netflix_titles.csv')
 
-# Function to fetch IMDb rating for a movie/TV show
+# Function to GET IMDb rating for a movie/TV show
 def get_imdb_rating(title):
     try:
-        # Make an API request to OMDB
+        # API request to OMDB
         url = f'http://www.omdbapi.com/?t={title}&apikey={api_key}'
         response = requests.get(url)
         data = response.json()
@@ -24,10 +24,8 @@ def get_imdb_rating(title):
         print(f"Error fetching rating for {title}: {e}")
         return None
 
-# Fetch IMDb ratings for each movie/TV show in the DataFrame
 df['imdb_rating'] = df['title'].apply(get_imdb_rating)
 
-# Save the DataFrame to a new CSV file
 df.to_csv('movies_with_imdb_ratings.csv', index=False)
 
 print("IMDb ratings fetched and saved to 'movies_with_imdb_ratings.csv'")
